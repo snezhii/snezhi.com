634bad443d3b95f0e75e89493a5b7f3139640b40
Snezhi <snezhina.racheva@sellpy.se>
2023-12-08 12:15:46 +0100

    alt text for all icons

--- home/public/socialIcons.js
+++ home/public/socialIcons.js
@@ -1,13 +1,22 @@
-export { default as CodePen } from './assets/icons/codepen.svg'
-export { default as Dev } from './assets/icons/dev.svg'
-export { default as Envelope } from './assets/icons/envelope.svg'
-export { default as GitHub } from './assets/icons/github.svg'
-export { default as LinkedIn } from './assets/icons/linkedin.svg'
+import { default as CodePen } from './assets/icons/codepen.svg'
+import { default as Dev } from './assets/icons/dev.svg'
+import { default as Envelope } from './assets/icons/envelope.svg'
+import { default as GitHub } from './assets/icons/github.svg'
+import { default as LinkedIn } from './assets/icons/linkedin.svg'
 
-export const SOCIAL_ICON = {
-  CODEPEN: 'CodePen',
-  DEV: 'Dev',
-  ENVELOPE: 'Envelope',
-  GITHUB: 'GitHub',
-  LINKEDIN: 'LinkedIn'
+export const SOCIAL_ICONS = {
+  CODEPEN: 'CODEPEN',
+  DEV: 'DEV',
+  ENVELOPE: 'ENVELOPE',
+  GITHUB: 'GITHUB',
+  LINKEDIN: 'LINKEDIN'
 }
+
+export const getSocialIcon = (kind) => ({
+  CODEPEN: CodePen,
+  DEV: Dev,
+  ENVELOPE: Envelope,
+  GITHUB: GitHub,
+  LINKEDIN: LinkedIn 
+}[kind])
+
